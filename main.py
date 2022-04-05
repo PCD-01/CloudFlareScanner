8cf10680f9b5312d0201805a55a81970a60bd752
PCD-01 <pcd-01@furry.club>
2022-04-05 17:28:40 +0800

    Update

--- main.py
+++ main.py
@@ -1,58 +1,44 @@
+from concurrent.futures import ThreadPoolExecutor
 import threading
 import time
 import requests
+import ipaddress
 
-ip1 = 1
-ip2 = 1
-ip3 = 1
+ip1 = 104
+ip2 = 21
+ip3 = 225
 ip4 = 1
 
 def scanip(url):
-    try:
-        r = requests.get(url,headers={'Host':'furrycloud.cf'},verify=False)
-        if("1145141919810" in r.text):
-            with open("test.txt","a") as f:
-                f.write(url)
-    except:
-        pass
+    #requests.packages.urllib3.disable_warnings()
+    r = requests.get(url,headers={'Host':'furrycloud.cf'}, timeout=1)
+    if("68621db880e735ba6ef49e04bb8b1f9c" in r.text):
+        print("[+] "+url+" 反代CloudFlare.jpg")
+        with open("ScanedIP.txt","a") as f:
+            f.write(url)
+            f.write("\n")
 
-def getip():
-    global ip1
-    global ip2
-    global ip3
-    global ip4
-    ip4+=1
-    if(ip4>255):
-        ip4=1
-        ip3+=1
-        if(ip3>255):
-            ip3=1
-            ip2+=1
-            if(ip2>255):
-                ip2=1
-                ip1+=1
-                if(ip1>255):
-                    exit()
-    scanip(str(ip1)+"."+str(ip2)+"."+str(ip3)+"."+str(ip4))
+def getip(a):
+    scanip(str(a))
 
 def get_thread():
+    time.sleep(3)
     while(1):
-        print(threading.active_count()) # 显示当前激活的线程数
-        print(threading.enumerate()) # 显示当前激活的线程
-        print(threading.current_thread()) # 当前运行的线程 
-        time.sleep(30)
+        if(threading.active_count()!=2):
+            print("当前线程数: "+str(threading.active_count()-1))
+        else:
+            print("当前线程数: 1")
+            break
+        time.sleep(1)
 
 def main():
+    network = ipaddress.ip_network('0.0.0.0/0')
     threads = input("需要的线程数:")
     need = int(threads)
-    thread = threading.Thread(target=get_thread,)
-    thread.start() # 开始该线程
-    while(1):
-        if(threading.active_count()<=need):
-            thread = threading.Thread(target=getip,)
-            thread.start() # 开始该线程
-        else:
-            time.sleep(5)
+    with ThreadPoolExecutor(need) as tpool:
+        tpool.submit(get_thread,)
+        for ip in network:
+            tpool.submit(getip,ip)
 
 if __name__ == '__main__':
     main()
